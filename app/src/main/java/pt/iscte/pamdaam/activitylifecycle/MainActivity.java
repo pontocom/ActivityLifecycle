8cec5405044ca522d0544edb24cba3d4660c2e43
Carlos Serrao <carlos.j.serrao@gmail.com>
2018-02-26 11:56:01 +0000

    Small change on the OnCreate function!

--- app/src/main/java/pt/iscte/pamdaam/activitylifecycle/MainActivity.java
+++ app/src/main/java/pt/iscte/pamdaam/activitylifecycle/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("APP", "ONCREATE");
     }
 
     // demonstrar os overrides no código
